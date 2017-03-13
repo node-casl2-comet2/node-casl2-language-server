527673f2a9271469306b904ef2c2850982e98317
Maxfield Walker <maxfield.walker@gmail.com>
2017-03-13 13:30:38 +0900

    [IntelliSense] GR completion

--- src/casl2.ts
+++ src/casl2.ts
@@ -5,7 +5,9 @@ import {
     TokenType, TokenInfo
 } from "@maxfield/node-casl2-core";
 import { Diagnostic, DiagnosticSeverity, CompletionItem, CompletionItemKind, Position } from "vscode-languageserver";
-import { instructionCompletionItems } from "./completion";
+import { instructionCompletionItems, grCompletionItems, indexGRCompletionItems } from "./completion";
+import { instructionMap, isAddressToken } from "@maxfield/node-casl2-core";
+import { ArgumentType } from "@maxfield/node-casl2-comet2-core-common";
 
 const casl2 = new Casl2();
 
@@ -75,6 +77,85 @@ export function completion(position: Position): Array<CompletionItem> {
         if (space(tokens, position) || label_space(tokens) || startOfLine(tokens)) {
             // 命令
             return instructionCompletionItems;
+        }
+
+        // GRの補完を出す条件
+        // 前提: 命令が入力されていて，その命令がとるオペランドがGRである
+        const instructionToken = tokens.filter(x => x.type == TokenType.TINSTRUCTION);
+        if (instructionToken.length > 0) {
+            const instToken = instructionToken[0];
+            const inst = instToken.value;
+            const info = instructionMap.get(inst);
+            if (info === undefined) throw new Error();
+            const beforeCursorTokens = getTokensBeforeCursor(tokens, position.character);
+
+            switch (info.argumentType) {
+                case ArgumentType.adr_r2:
+                    // e.g. JUMP L1, |
+                    if (beforeCursorTokens.length >= 4) {
+                        const [l1, l2, l3, l4] = beforeCursorTokens.slice(beforeCursorTokens.length - 4);
+                        if (l1 == instToken && l2.type == TokenType.TSPACE && isAddressToken(l3.type) && l4.type == TokenType.TCOMMASPACE) {
+                            return indexGRCompletionItems;
+                        }
+                    }
+                    break;
+
+
+                case ArgumentType.r:
+                    // e.g. POP |
+                    if (beforeCursorTokens.length >= 2) {
+                        const [l1, l2] = beforeCursorTokens.slice(beforeCursorTokens.length - 2);
+                        if (l1 == instToken && l2.type == TokenType.TSPACE) {
+                            return grCompletionItems;
+                        }
+                    }
+                    break;
+
+
+                case ArgumentType.r1_r2:
+                    // r1, r2パターンのみの命令は存在しないので
+                    // r1_r2_OR_r1_adr_r2で処理されるはず
+                    throw new Error();
+
+
+                case ArgumentType.r1_adr_r2:
+                    // e.g. SLA |
+                    if (beforeCursorTokens.length >= 2) {
+                        const [l1, l2] = beforeCursorTokens.slice(beforeCursorTokens.length - 2);
+                        if (l1 == instToken && l2.type == TokenType.TSPACE) {
+                            return grCompletionItems;
+                        }
+                    }
+                    // e.g. SLA GR1, 1, |
+                    if (beforeCursorTokens.length >= 6) {
+                        const [l1, l2, l3, l4, l5, l6] = beforeCursorTokens.slice(beforeCursorTokens.length - 6);
+                        if (l1 == instToken && l2.type == TokenType.TSPACE && l3.type == TokenType.TGR && l4.type == TokenType.TCOMMASPACE
+                            && isAddressToken(l5.type) && l6.type == TokenType.TCOMMASPACE) {
+                            return indexGRCompletionItems;
+                        }
+                    }
+                    break;
+
+
+                case ArgumentType.r1_r2_OR_r1_adr_r2:
+                    // e.g. ADDA |
+                    if (beforeCursorTokens.length >= 2) {
+                        const [l1, l2] = beforeCursorTokens.slice(beforeCursorTokens.length - 2);
+                        if (l1 == instToken && l2.type == TokenType.TSPACE) {
+                            return grCompletionItems;
+                        }
+                    }
+                    // e.g. ADDA GR1, |
+                    if (beforeCursorTokens.length >= 4) {
+                        const [l1, l2, l3, l4] = beforeCursorTokens.slice(beforeCursorTokens.length - 4);
+                        if (l1 == instToken && l2.type == TokenType.TSPACE &&
+                            l3.type == TokenType.TGR && l4.type == TokenType.TCOMMASPACE) {
+                            // TODO: ラベルが来る可能性もあるので，ラベルも補完候補に含める
+                            return grCompletionItems;
+                        }
+                    }
+                    break;
+            }
         }
     }
 
--- src/completion.ts
+++ src/completion.ts
@@ -1,12 +1,14 @@
 "use strict";
 
 import { CompletionItem, CompletionItemKind } from "vscode-languageserver";
-import { instructionsInfo, InstructionInfo } from "@maxfield/node-casl2-comet2-core-common";
+import { instructionsInfo, InstructionInfo, grToString, GR, grsInfo, GRInfo, GRType } from "@maxfield/node-casl2-comet2-core-common";
 import * as _ from "lodash";
 
-export const instructionCompletionItems: Array<CompletionItem> = create();
+export const instructionCompletionItems: Array<CompletionItem> = createInstructionCompletionItems();
+export const grCompletionItems: Array<CompletionItem> = createGRCompletionItems();
+export const indexGRCompletionItems: Array<CompletionItem> = createIndexRegisterCompletionItems();
 
-function create(): Array<CompletionItem> {
+function createInstructionCompletionItems(): Array<CompletionItem> {
     const items = instructionsInfo.map(convertInstructionInfoToCompletionItem);
     // 重複を除く
     return _.uniqBy(items, x => x.label);
@@ -19,4 +21,22 @@ function convertInstructionInfoToCompletionItem(info: InstructionInfo): Completi
         detail: info.instructionName + "命令",
         documentation: info.documentation
     };
+}
+
+function convertGRInfoToCompletionItem(info: GRInfo): CompletionItem {
+    return {
+        label: info.name,
+        kind: CompletionItemKind.Property,
+        documentation: info.documentation
+    };
+}
+
+function createGRCompletionItems(): Array<CompletionItem> {
+    return grsInfo.map(convertGRInfoToCompletionItem);
+}
+
+function createIndexRegisterCompletionItems(): Array<CompletionItem> {
+    return grsInfo
+        .filter(x => x.type & GRType.UsedAsIndexRegister)
+        .map(convertGRInfoToCompletionItem);
 }
